dcdb5296c84dec0ec9d5567d6b80729042cdf7aa
Andreea Lupu <0607andreea.ro@gmail.com>
2026-07-24 13:27:23 +0300

    gpt2 medium L = 760; m = 20

--- h2/h2_train_sweep.py
+++ h2/h2_train_sweep.py
@@ -5,8 +5,8 @@ import subprocess, os, time, torch, json, math
 
 # RUN_ORDER  = [0, 16, 32, 64, 80, 100, 256, 512, 760]
 
-RUN_ORDER  = [16, 32]
-M_VALUES = [1]
+RUN_ORDER  = [760]
+M_VALUES = [20]
 TASK      = "wikitext2"
 
 results         = []
